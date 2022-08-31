deeea16719d9a5609de25b4ff1e6ebf92089d05d
Alex Nicolaou <alex@stockgamblers.com>
2022-08-31 00:33:48 -0400

    Test out dev server updating.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>The All New TODO</h1>
+<p>A hopefully simpler, more robust, TODO list app.</p>
