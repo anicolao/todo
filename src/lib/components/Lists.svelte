d1bfb8aa885ee0f8aff8743106b0690ffd58fed4
Andrew Blackledge <andrew@stockgamblers.com>
2022-09-07 22:58:36 -0400

    Permissions for creating and editing lists.
    Change the firebase rules for list creation and editing, to be less
    specific, but still as secure.
    
    In the app, when a list is created, add the user to the list of editors
    of that list, so that the user has the firebase permissions to save the
    list's initial action list.

--- src/lib/components/Lists.svelte
+++ src/lib/components/Lists.svelte
@@ -2,6 +2,7 @@
 	import firebase from '$lib/firebase';
 	import { create_list, delete_list, rename_list } from '$lib/components/lists';
 	import { store } from '$lib/store';
+	import { collection, doc, setDoc } from 'firebase/firestore';
 
 	let errorMessage: string;
 
@@ -9,13 +10,39 @@
 
 	function createNewList(event: any) {
 		errorMessage = '';
-		let name = event.srcElement.value;
-		firebase.dispatch(create_list({ id: crypto.randomUUID(), name: name }));
+		const name = event.srcElement.value;
+		const id = crypto.randomUUID();
+		firebase.dispatch(create_list({ id, name }));
 		listName = undefined;
 	}
 
+	$: latestListCount = $store.lists.visibleLists.length;
+	let lastListCount = 0;
+	$: if (latestListCount > lastListCount) {
+		let numNewLists = latestListCount - lastListCount;
+		for (let i = 0; i < numNewLists; ++i) {
+			const user = store.getState().auth;
+			const id = $store.lists.visibleLists[i];
+			const name = $store.lists.listIdToList[id];
+			console.log('Create new list for ', id);
+			setDoc(doc(firebase.firestore, 'editors', id, user.uid, 'editor'), { email: user.email })
+				.then(() => {
+					console.log('Create new actions for ', id);
+					return setDoc(doc(firebase.firestore, 'lists', id, 'actions', 'name'), {
+						...rename_list({ id, name }),
+						timestamp: 0
+					});
+				})
+				.catch((message) => {
+					console.error(message);
+				});
+		}
+		lastListCount = latestListCount;
+	}
+
 	function renameList(listId: string) {
-		return (event: any) => firebase.dispatch(rename_list({ id: listId, name: event.srcElement.value }));
+		return (event: any) =>
+			firebase.dispatch(rename_list({ id: listId, name: event.srcElement.value }));
 	}
 
 	function deleteList(listId: string) {
@@ -24,7 +51,13 @@
 </script>
 
 <label for="newListInput">New List:</label>
-<input type="text" id="newListInput" bind:value={listName} placeholder="list name" on:change={createNewList} />
+<input
+	type="text"
+	id="newListInput"
+	bind:value={listName}
+	placeholder="list name"
+	on:change={createNewList}
+/>
 <ul>
 	{#each $store.lists.visibleLists as listId}
 		<li>
