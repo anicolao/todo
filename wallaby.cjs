1c70375e5db918a7a45ff890aa243134598c4a2e
Andrew Blackledge <andrew@stockgamblers.com>
2022-10-19 16:14:53 -0400

    Add wallaby config file.

---
+++ wallaby.cjs
@@ -0,0 +1,10 @@
+module.exports = function (wallaby) {
+	return {
+		autoDetect: true,
+		maxConsoleMessagesPerTest: 100000,
+		maxTraceSteps: 4000000,
+		tests: [
+			'tests/**/*.unit.ts',
+		]
+	};
+};
