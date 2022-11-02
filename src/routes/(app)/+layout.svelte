49d51305260a7ab63d9ca4ef9c901caf34c80ff0
Andrew Blackledge <andrew@stockgamblers.com>
2022-11-02 13:30:12 -0400

    Revert "Formatting and unused css."
    This reverts commit c499cf5bf23f5dc9359b889c87c96fd48f075666.

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -184,4 +184,18 @@
 	.desk-margin {
 		margin-left: 256px;
 	}
+
+	.card-display {
+		margin: 25%;
+		margin-top: 5%;
+	}
+
+	p {
+		padding-top: 0.5em;
+	}
+
+	h2 {
+		font-family: 'Roboto', sans-serif;
+		font-size: large;
+	}
 </style>
