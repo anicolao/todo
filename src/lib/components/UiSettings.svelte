3fbbb8d13e917e0ef957f14f2661ca08e7366ebc
Andrew Blackledge <andrew@stockgamblers.com>
2023-01-20 16:57:15 -0500

    Improve Profile page: use Paper as background.
    Use Paper as background for items in the page as there could be a busy
    background image that makes plain text hard to read.

--- src/lib/components/UiSettings.svelte
+++ src/lib/components/UiSettings.svelte
@@ -3,7 +3,7 @@
 	import { set_background_url } from '$lib/components/UiSettings';
 	import firebase from '$lib/firebase';
 	import { store } from '$lib/store';
-	import Paper, { Content } from '@smui/paper';
+	import Paper from '@smui/paper';
 
 	function setBackground(event: { currentTarget: HTMLInputElement }) {
 		let imgUrl = event.currentTarget.value;
@@ -12,19 +12,19 @@
 </script>
 
 <div>
-	<label for="url">Background image url:</label>
-	<input
-		type="url"
-		id="url"
-		placeholder="image URL"
-		value={$store.uiSettings.backgroundUrl || ''}
-		on:input={setBackground}
-	/>
-	{#if $store.uiSettings.backgroundUrl}
-		<Paper elevation={24}>
-			<Content>
+	<Paper style="opacity: 0.8;">
+		<label for="url">Background image url:</label>
+		<input
+			type="url"
+			id="url"
+			placeholder="image URL"
+			value={$store.uiSettings.backgroundUrl || ''}
+			on:input={setBackground}
+		/>
+		{#if $store.uiSettings.backgroundUrl}
+			<Paper elevation={24} style="padding: 1em; margin: 1em;">
 				<img src={$store.uiSettings.backgroundUrl} alt="background for the app" height="300" />
-			</Content>
-		</Paper>
-	{/if}
+			</Paper>
+		{/if}
+	</Paper>
 </div>
