0c9cbb954c0eab610170186f682f4a1d1b81a757
Andrew Blackledge <andrew@stockgamblers.com>
2023-01-25 15:40:47 -0500

    Remove old landing page.

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,18 +1,10 @@
 <script>
+	import { goto } from '$app/navigation';
+
 	console.log('routes/(app)/+page.svelte');
-	import Login from '$lib/components/Login.svelte';
-	import { store } from '$lib/store';
-	import UiSettings from '$lib/components/UiSettings.svelte';
-	import Lists from '$lib/components/Lists.svelte';
+	goto('/profile');
 </script>
 
 <div>
 	<h1>The All New TODO</h1>
-	<p>A hopefully simpler, more robust, TODO list app.</p>
-
-	{#if $store.auth.signedIn === true}
-		<UiSettings />
-		<Lists />
-	{/if}
-	<Login />
 </div>
