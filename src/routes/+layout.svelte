6abb171e353ffb8eded0f1717fe33c365f7e9230
Andrew Blackledge <andrew@stockgamblers.com>
2023-01-25 15:26:32 -0500

    Move auth code into routes/+layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,57 @@
 <script lang="ts">
 	console.log('src/routes/+layout.svelte');
 	import { goto } from '$app/navigation';
+	import { signed_in, signed_out } from '$lib/components/auth';
+	import firebase from '$lib/firebase';
 	import { store } from '$lib/store';
+	import { onAuthStateChanged } from 'firebase/auth';
+	import { doc, setDoc } from 'firebase/firestore';
+
+	let count = 0;
+	function load() {
+		console.log('/+layout.ts load function', count++);
+		const auth = firebase.auth;
+		const gAuthProvider = firebase.google_auth_provider;
+		console.log('/+layout.ts set up auth state callback');
+		const unsubAuth = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				console.log('/+layout.ts auth callback for user ', { user });
+				const uid = user.uid;
+				console.log('/+layout.ts onAuthStateChanged   sign in ');
+				store.dispatch(
+					signed_in({
+						uid: uid,
+						name: user.displayName,
+						email: user.email,
+						photo: user.photoURL,
+						signedIn: true,
+						authMessage: ''
+					})
+				);
+				if (user.email) {
+					// always true
+					setDoc(doc(firebase.firestore, 'users', user.email), {
+						uid: user.uid,
+						name: user.displayName,
+						email: user.email,
+						photo: user.photoURL,
+						activity_timestamp: new Date().getTime()
+					}).catch((message) => {
+						// TODO: Surface this error state in the UI.
+						console.error(message);
+					});
+				}
+			} else {
+				console.log('/+layout.ts onAuthStateChanged   sign out ');
+				store.dispatch(signed_out());
+			}
+		});
+		// onDestroy(unsubAuth);
+	}
+	load();
 
 	$: if ($store.auth.signedIn === false) {
-		console.log('...redirect to /login', {'signedIn?': $store.auth.signedIn});
+		console.log('...redirect to /login', { 'signedIn?': $store.auth.signedIn });
 		goto('/login');
 	}
 </script>
