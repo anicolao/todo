7d7430f6ce30f86805d6d1110a7cf309cb990f92
Andrew Blackledge <andrew@stockgamblers.com>
2023-01-27 20:45:25 -0500

    A FilterMenu item is active if url matches its name.
    The FilterMenu items are the list names like 'All', 'Starred', etc.
    
    Before this change, there was no indication which FilterMenu was
    currently active because the background of the one in use wasn't
    changed.
    
    Set a menu item 'active' so it can draw with a different background
    colour to show that it is the item that is selected.  This is done
    by looking at the url, and seeing if it ends with '/all', or
    '/starred', etc.

--- src/lib/components/FilterMenu.svelte
+++ src/lib/components/FilterMenu.svelte
@@ -1,24 +1,36 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import List, { Item } from '@smui/list';
 
 	export let setActive: (name: string) => void;
+
+	$: pagePath = $page.url.pathname;
 </script>
 
 <List>
-	<Item href="javascript:void(0)" on:click={() => setActive('all')}
+	<Item href="javascript:void(0)" on:click={() => setActive('all')} activated={pagePath === '/all'}
 		><img src="all.svg" alt="all" />All</Item
 	>
-	<Item href="javascript:void(0)" on:click={() => setActive('starred')}
-		><img src="star.svg" alt="starred" />Starred</Item
+	<Item
+		href="javascript:void(0)"
+		on:click={() => setActive('starred')}
+		activated={pagePath === '/starred'}><img src="star.svg" alt="starred" />Starred</Item
 	>
-	<Item href="javascript:void(0)" on:click={() => setActive('bydate')}
-		><img src="date.svg" alt="by date" />By Date</Item
+	<Item
+		href="javascript:void(0)"
+		on:click={() => setActive('bydate')}
+		activated={pagePath === '/bydate'}><img src="date.svg" alt="by date" />By Date</Item
 	>
-	<Item href="javascript:void(0)" on:click={() => setActive('completed')}
+	<Item
+		href="javascript:void(0)"
+		on:click={() => setActive('completed')}
+		activated={pagePath === '/completed'}
 		><img class="checkmark" src="checkmark.svg" alt="by date" />Completed</Item
 	>
-	<Item href="javascript:void(0)" on:click={() => setActive('search')}
-		><img src="search.svg" alt="search" />Search</Item
+	<Item
+		href="javascript:void(0)"
+		on:click={() => setActive('search')}
+		activated={pagePath === '/search'}><img src="search.svg" alt="search" />Search</Item
 	>
 </List>
 
