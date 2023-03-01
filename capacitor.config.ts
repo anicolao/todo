ead1e23d76418fadb384baae7e7ffa52fa2fc016
Alex Nicolaou <alex@stockgamblers.com>
2023-03-01 13:49:26 -0500

    Add a first cut of an iOS capacitor app to Todo.
    This turned out to have lots of gotchas:
    
    - capacitor.config.ts had to list the auth plugin
    - Podfile had to be modified to list the auth plugin
    - Info.plist had to have a URL added to it according to an error message
      when the app crashed
    - AppDelegate.swift needed a line of code added to it to initialize
      firebase
    - GoogleServiceInfo plist had to be downloaded from firebase's cloud
      console
    
    As well as some more-or-less well documented steps:
    
    npm i @capacitor/ios
    npx cap add ios
    npx cap sync
    npx cap open ios
    
    For each rebuild, it should suffice to do:
    
    npx cap sync
    npx cap open ios
    
    and rebuild inside Xcode's GUI.

---
+++ capacitor.config.ts
@@ -0,0 +1,16 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stockgamblers.todo',
+  appName: 'Todo',
+  webDir: 'build',
+  bundledWebRuntime: false,
+  plugins: {
+    FirebaseAuthentication: {
+      skipNativeAuth: true,
+      providers: ["google.com"],
+    },
+  },
+};
+
+export default config;
