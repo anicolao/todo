293a126841c2c3183b265f223cbf0aab309f600d
Alex Nicolaou <alex@stockgamblers.com>
2023-03-08 12:17:07 -0500

    Vanilla checkin of initial capacitor setup.
    These changes come from the following commands:
    
    npm i @capacitor/core
    npm i -D @capacitor/cli
    npx cap init
    
    Taken from the getting started page here:
    
    https://capacitorjs.com/docs/getting-started

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.stockgamblers.todo',
+  appName: 'Todo',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
