031b8cb9a0805e4fd7b01b59740a9c23925e0169
Andrew Blackledge <andrew@stockgamblers.com>
2023-03-22 14:32:03 -0400

    Import due dates, repeating items, and lost items.
    As part of verifying the imported items, we found several that were
    not part of the doubly-linked list in the old system.  We also import
    these, appended with "LOST_ITEM".

--- src/routes/(app)/import/+page.svelte
+++ src/routes/(app)/import/+page.svelte
@@ -4,12 +4,13 @@
 	import firebase from '$lib/firebase';
 	import { dispatch } from '$lib/components/ActionLog';
 	import { store } from '$lib/store';
-	import { create_item } from '$lib/components/items';
-	import { complete_item, star_item } from '$lib/components/items';
+	import { create_item, type DueDate } from '$lib/components/items';
+	import { complete_item, star_item, set_due_date } from '$lib/components/items';
 
 	function importItem(listItem: any, list_id: string, id: string) {
 		const completed = listItem.extendedProperties.shared.done;
 		const starred = listItem.extendedProperties.shared.starred;
+		const dueDate = listItem.extendedProperties.shared.dueDate;
 		const description = listItem.description;
 		if (!description) {
 			console.error(`List Item: ${listItem.description} ${completed}`);
@@ -30,6 +31,48 @@
 				$store.auth.uid,
 				star_item({ list_id, id, starred, star_timestamp })
 			);
+		}
+		if (dueDate) {
+			/*
+		export interface DueDate {
+			year: number; // 4-digit year, eg: 2022.
+			month: number; // Month [1..12].
+			day: number; // Day of month [1..31].
+			repeats?: RepeatInfo;
+		}
+
+		export interface RepeatInfo {
+			type: RepeatType;
+			every: number;
+		}
+
+		export enum RepeatType {
+			NONE = 'none',
+			DAILY = 'daily', // every N day(s)
+			WEEKLY = 'weekly', // on dayOfWeek (from 'due') AND every N weeks
+			MONTHLY = 'monthly', // on day N (from 'due')    AND every N months
+			YEARLY = 'yearly', // on Month Day (Jan 1 .. Jan 31, Dec 1 .. Dec 31)
+			WEEKDAYS = 'weekdays'
+		}
+		*/
+
+			const split = dueDate.split('-').map((x: string) => parseInt(x));
+			const due_date: DueDate = {
+				year: split[0],
+				month: split[1],
+				day: split[2]
+			};
+
+			const repeats = listItem.extendedProperties.shared.repeats;
+			if (repeats) {
+				const every = +listItem.extendedProperties.shared.repeatInterval || 1;
+				due_date.repeats = {
+					type: repeats,
+					every
+				};
+			}
+
+			dispatch('lists', list_id, $store.auth.uid, set_due_date({ list_id, id, due_date }));
 		}
 		if (completed) {
 			let completed_time = listItem.extendedProperties.shared.timestamps.done;
@@ -52,10 +95,23 @@
 			console.log(`Importing list ${listName}`);
 			const todos = listData.todos;
 			const anchor = listData.anchor_id;
-			let current = todos[anchor].extendedProperties.shared.next;
+			const missedTodos = { ...todos };
+			delete missedTodos[anchor];
+			let current = todos[anchor].extendedProperties.shared.prev;
+			let count = 0;
 			while (current !== anchor) {
 				importItem(todos[current], listId, current);
-				current = todos[current].extendedProperties.shared.next;
+				delete missedTodos[current];
+				++count;
+				current = todos[current].extendedProperties.shared.prev;
+			}
+			const expected = Object.keys(todos).length - 1;
+			if (count !== expected) {
+				console.error(count + ' was not the expected length of ' + expected);
+				for (let m in missedTodos) {
+					missedTodos[m].description += ' LOST_ITEM';
+					importItem(missedTodos[m], listId, m);
+				}
 			}
 		});
 	}
