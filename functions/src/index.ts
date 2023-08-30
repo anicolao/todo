6db2f3c97d3367d03de27e9356326e691969b772
Andrew Blackledge <andrew@stockgamblers.com>
2023-08-30 15:16:24 -0400

    Remove pushover and tidy notifications code.
    Store notification tokens in firestore at notifications/${id}/tokens
    to avoid a lookup through the email field.
    
    Remove pushover now that notifications are working the firebase way.
    
    Make a notifyUser() function that does the work of notifying.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,31 +4,6 @@ import { AnyAction } from '@reduxjs/toolkit';
 
 admin.initializeApp();
 
-// post utility function
-async function post(url: string, data: any): Promise<any> {
-	const response = await fetch(url, {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/x-www-form-urlencoded'
-		},
-		redirect: 'follow',
-		referrerPolicy: 'no-referrer',
-		body: new URLSearchParams(data).toString()
-	});
-	return response.json();
-}
-
-const pushover = async function (user: string, message: string) {
-	const token = 'adgzouoeu2zkv7pmv7tdnd8wwcyc87';
-	const html = 1;
-	const priority = 2;
-	const retry = 30;
-	const expire = 300;
-	const sound = 'bugle';
-	const params = { token, user, message, html, priority, retry, expire, sound };
-	return post('https://api.pushover.net/1/messages.json', params);
-};
-
 exports.users = functions.https.onRequest(async (req, res) => {
 	const db = admin.firestore();
 	const users = db.collection('users');
@@ -44,7 +19,7 @@ function makeNotification(action: AnyAction) {
 		title = 'New Todo Item';
 	} else if (action.type === 'complete_item') {
 		title = 'Todo Completed';
-	} else if (action.type === 'incoming_request') {
+	} else if (action.type === 'accept_pending_share') {
 		title = 'List Shared';
 	}
 	return {
@@ -53,6 +28,36 @@ function makeNotification(action: AnyAction) {
 		image
 	};
 }
+
+async function notifyUser(id: string, currentAction: AnyAction): Promise<any> {
+	const promises: Promise<any>[] = [];
+	const db = admin.firestore();
+	const notificationTokens = await db.collection(`notifications/${id}/tokens`).get();
+	const tokens: string[] = [];
+	notificationTokens.forEach((doc) => tokens.push(doc.id));
+	if (tokens.length) {
+		const notification = makeNotification(currentAction);
+		const message = {
+			data: { action: JSON.stringify(currentAction) },
+			notification,
+			tokens
+		};
+		admin
+			.messaging()
+			.sendEachForMulticast(message)
+			.then((r: any) => {
+				console.log('Sent ' + r.successCount + ' Failed ' + r.failureCount);
+				r.responses.forEach((resp: any, index: number) => {
+					if (!resp.success) {
+						console.log('Multicase send failed for token: ', tokens[index], resp.error);
+						promises.push(db.doc(`notifications/${id}/tokens/${tokens[index]}`).delete());
+					}
+				});
+			});
+	}
+	return Promise.all(promises);
+}
+
 exports.onTodoItemChanged = functions.firestore
 	.document('lists/{listId}/actions/{action}')
 	.onCreate(async (change, _context) => {
@@ -75,39 +80,7 @@ exports.onTodoItemChanged = functions.firestore
 					console.log(id);
 					if (id !== currentAction.creator) {
 						console.log('try to notify', id);
-						const pushKeyDoc = (await db.doc(`notifications/${id}`).get()).data();
-						const pushKey = pushKeyDoc?.pushoverKey;
-						if (pushKey) {
-							console.log('key', pushKey);
-							promises.push(pushover(pushKey, currentAction.type));
-						}
-						const editorEmailDoc = (await db.doc(`editors/${listId}/${id}/editor`).get()).data();
-						const email = editorEmailDoc?.email;
-						if (email) {
-							const notificationTokens = await db.collection(`users/${email}/tokens`).get();
-							const tokens: string[] = [];
-							notificationTokens.forEach((doc) => tokens.push(doc.id));
-							if (tokens.length) {
-								const notification = makeNotification(currentAction as AnyAction);
-								const message = {
-									data: { action: JSON.stringify(currentAction) },
-									notification,
-									tokens
-								};
-								admin
-									.messaging()
-									.sendEachForMulticast(message)
-									.then((r: any) => {
-										console.log('Sent ' + r.successCount + ' Failed ' + r.failureCount);
-										r.responses.forEach((resp: any, index: number) => {
-											if (!resp.success) {
-												console.log('Multicase send failed for token: ', tokens[index], resp.error);
-												promises.push(db.doc(`users/${email}/tokens/${tokens[index]}`).delete());
-											}
-										});
-									});
-							}
-						}
+						promises.push(notifyUser(id, currentAction as AnyAction));
 					}
 				}
 			}
@@ -130,9 +103,14 @@ exports.onLogin = functions.firestore
 		const user = change.after.data();
 		// console.log(JSON.stringify(user));
 		if (user?.notificationToken) {
-			const userDocPath = change.after.ref;
-			const tokenList = userDocPath.collection('tokens');
-			promises.push(tokenList.doc(`${user.notificationToken}`).set({ timestamp: Date.now() }));
+			const id = change.after.data()?.uid;
+			if (id !== undefined) {
+				const db = admin.firestore();
+				const notificationTokenList = db.collection(`notifications/${id}/tokens`);
+				promises.push(
+					notificationTokenList.doc(`${user.notificationToken}`).set({ timestamp: Date.now() })
+				);
+			}
 		}
 		return Promise.all(promises);
 	});
@@ -145,23 +123,7 @@ exports.onTodoListShared = functions.firestore
 		console.log(JSON.stringify(currentAction));
 		if (currentAction.type === 'accept_pending_share') {
 			const targetUserId = currentAction.target;
-
-			const db = admin.firestore();
-			const pushKeyDoc = (await db.doc(`notifications/${targetUserId}`).get()).data();
-			const pushKey = pushKeyDoc?.pushoverKey;
-			if (pushKey) {
-				console.log('key', pushKey);
-				promises.push(pushover(pushKey, currentAction.type));
-			}
+			promises.push(notifyUser(targetUserId, currentAction as AnyAction));
 		}
 		return Promise.all(promises);
 	});
-
-/*
-const pushoverAndrew = "u5f5ze6p5hvv3k6tprm7s5qnuh4csi";
-
-exports.helloWorld = functions.https.onRequest(async (req, res) => {
-	res.json({ hello: "Hello World!" });
-	return pushover(pushoverAndrew, "Subsuquent notifications from code");
-})
-*/
